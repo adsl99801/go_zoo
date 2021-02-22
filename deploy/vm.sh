cff078e0c2e7305b5cf11819959fc635fbaa28c6
keithlin <keithlin@awt-system.com.tw>
2021-02-22 16:56:52 +0800

    config

---
+++ deploy/vm.sh
@@ -0,0 +1,30 @@
+#PostgreSQL
+# Create the file repository configuration:
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+# Install the latest version of PostgreSQL.
+# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
+sudo apt-get -y install postgresql-13
+
+#go
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get update
+sudo apt install golang-go
+
+#nginx
+sudo apt install curl gnupg2 ca-certificates lsb-release
+echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
+sudo apt update
+sudo apt install nginx
+
+#git clone https://github.com/adsl99801/go_zoo
+#bash go_zoo/deploy/vm.sh
+cd /root/go_zoo
+cp deploy/go_zoo.conf /etc/nginx/conf.d
+cp deploy/go_zoo.service /lib/systemd/system/go_zoo.service
+env GOOS=linux GOARCH=amd64 go build .
+systemctl go_zoo start
+systemctl go_zoo enable
