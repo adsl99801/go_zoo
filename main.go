6df1d5cbf188d7764faf9c4a44b53476b759ae26
keithlin@awt-system.com.tw <keithlin@awt-system.com.tw>
2022-07-16 22:56:01 +0800

    go index.html

--- main.go
+++ main.go
@@ -11,13 +11,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.GET("/igAuth", ig.IgAuth)
-	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-        Root:   "web/dist/puppysfun/",
-        Index: "index.html",
-        Browse: true,
-        HTML5:  true,
-    }))
-	e.Static("/", "web/dist/puppysfun/")
-	e.File("/*", "web/dist/puppysfun/index.html")
+	e.Static("/", "public")
+	e.File("/", "public/index.html")
 	e.Logger.Fatal(e.Start(":8080"))
 }
