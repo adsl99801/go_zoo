4f605a6b4c74440820e9e0bd8b0b9ddaf42c7383
keithlin@awt-system.com.tw <keithlin@awt-system.com.tw>
2022-07-17 23:08:22 +0800

    vm.sh

--- deploy/vm.sh
+++ deploy/vm.sh
@@ -19,6 +19,7 @@ echo "deb http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" |
     sudo tee /etc/apt/sources.list.d/nginx.list
 sudo apt update
 sudo apt install nginx
+sudo rm /etc/nginx/sites-enabled/default
 sudo cp deploy/go_zoo.conf /etc/nginx/nginx.conf/
 sudo systemctl reload nginx
 sudo systemctl start nginx
@@ -26,17 +27,17 @@ sudo systemctl start nginx
 scp -r web/dist/puppysfun keith@192.168.0.104:/home/keith/puppys.fun
 sudo cp -r puppys.fun /var/www/
 
-cp deploy/godaddy.sh /etc/cron.daily/godaddy
-cp deploy/limits.conf /etc/security/limits.conf
-cp deploy/sshd_config /etc/ssh/sshd_config
+sudo cp deploy/godaddy.sh /etc/cron.daily/godaddy
+sudo cp deploy/limits.conf /etc/security/limits.conf
+# ssh-copy-id keith@192.168.0.104
+sudo cp deploy/sshd_config /etc/ssh/sshd_config
 #git clone https://github.com/adsl99801/go_zoo
 #bash go_zoo/deploy/vm.sh
 # 環境需要4G
 cd /home/keith/go_zoo
-rm /etc/nginx/sites-enabled/default
-cp deploy/go_zoo.conf /etc/nginx/conf.d
-systemctl reload nginx
-cp deploy/go_zoo.service /lib/systemd/system/go_zoo.service
+# cp deploy/go_zoo.conf /etc/nginx/conf.d
+# systemctl reload nginx
+sudo cp deploy/go_zoo.service /lib/systemd/system/go_zoo.service
 env GOOS=linux GOARCH=amd64 go build .
-systemctl start go_zoo
-systemctl enable go_zoo
+sudo systemctl start go_zoo
+sudo systemctl enable go_zoo
