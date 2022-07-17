4bab29c06a47352ab0a088ee80b2f5adf30ecfb8
keithlin@awt-system.com.tw <keithlin@awt-system.com.tw>
2022-07-17 22:45:32 +0800

    vm.sh

--- deploy/vm.sh
+++ deploy/vm.sh
@@ -1,17 +1,17 @@
+timedatectl set-timezone Asia/Taipei
+
+# /etc/systemd/system/enable-console-blanking.service
+sudo cp deploy/enable-console-blanking.service /etc/systemd/system/
 #PostgreSQL
-# Create the file repository configuration:
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-# Import the repository signing key:
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-# Install the latest version of PostgreSQL.
-# If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
-sudo apt-get -y install postgresql-13
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get -y install postgresql-13
 
 #go
-#sudo add-apt-repository ppa:longsleep/golang-backports
-#sudo apt-get update
-#sudo apt install golang-go
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get update
+sudo apt install golang-go
 
 #nginx
 sudo apt install curl gnupg2 ca-certificates lsb-release
@@ -19,6 +19,12 @@ echo "deb http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" |
     sudo tee /etc/apt/sources.list.d/nginx.list
 sudo apt update
 sudo apt install nginx
+sudo cp deploy/go_zoo.conf /etc/nginx/nginx.conf/
+sudo systemctl reload nginx
+sudo systemctl start nginx
+
+scp -r web/dist/puppysfun keith@192.168.0.104:/home/keith/puppys.fun
+sudo cp -r puppys.fun /var/www/
 
 #git clone https://github.com/adsl99801/go_zoo
 #bash go_zoo/deploy/vm.sh
@@ -31,7 +37,3 @@ cp deploy/go_zoo.service /lib/systemd/system/go_zoo.service
 env GOOS=linux GOARCH=amd64 go build .
 systemctl start go_zoo
 systemctl enable go_zoo
-
-timedatectl set-timezone Asia/Taipei
-# scp -r web/dist/puppysfun keith@192.168.0.104:/home/keith/puppys.fun
-# sudo cp -r puppys.fun /var/www/
